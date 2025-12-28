2ff9ab099b1c439014221d810151cfa1f76e7581
Tanvi <tanvimendiratta7@gmail.com>
2025-12-28 10:40:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,30 +10,27 @@ let b5 = ["true","false"]
 const app = express()
 const port = 3010
 await mongoose.connect("mongodb://localhost:27017/random")
-let random = new RandomNoGen();
+
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-  res.render('t');
-})
 
 app.get('/generate', async (req, res) => {
-    let a1 = Math.ceil(0+Math.random()*4 );
-let a2 = Math.ceil(0+Math.random()*4 );
-let a3 = Math.ceil(0+Math.random()*4 );
-let a4 = Math.ceil(0+Math.random()*4 );
-let a5 = Math.ceil(0+Math.random()*2 );
-
+   
 await RandomNoGen.deleteMany({});
 for(let i=0;i<10;i++){
-random = await RandomNoGen.create({
-    name: `${b1[a1-1]}`,
-    salary: `${b4[a4-1]}`,
-    language: `${b2[a2-1]}`,
-    city: `${b3[a3-1]}`,
-    isManager: `${b5[a5-1]}`
+  const a1 = Math.floor(Math.random() * b1.length);
+    const a2 = Math.floor(Math.random() * b2.length);
+    const a3 = Math.floor(Math.random() * b3.length);
+    const a4 = Math.floor(Math.random() * b4.length);
+    const a5 = Math.floor(Math.random() * b5.length);
+await RandomNoGen.create({
+    name: `${b1[a1]}`,
+    salary: `${b4[a4]}`,
+    language: `${b2[a2]}`,
+    city: `${b3[a3]}`,
+    isManager: `${b5[a5]}`
 }
 )}
-await random.save();
+
 res.render('index');
 
 
